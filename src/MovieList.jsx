043ead704ecbbc986f81a1201cdcbf7c06797a4e
Amari May <164654924+AmariMay1213@users.noreply.github.com>
2025-06-15 21:12:02 -0700

    Create MovieList.jsx
    Created Movie List.jsx so that a page full of movies appear within a list

---
+++ src/MovieList.jsx
@@ -0,0 +1,22 @@
+import MovieCard from "../MOVIECARD/MovieCard";
+import "./MovieList.css";
+
+// MovieList receives the movie data from main.jsx as a prop
+const MovieList = ({ movies, onMovieClick }) => {
+  // If the array is empty (e.g. search returned nothing), show a message
+  if (movies.length === 0) {
+    return <p className="no-results">No movies found.</p>;
+  }
+
+  return (
+    <div className="movies-list">
+      {/* Loop through all movies passed in and render a MovieCard for each */}
+      {movies.map((item, idx) => (
+        <MovieCard key={idx} movie={item} onClick={onMovieClick}/>
+      ))}
+    </div>
+    
+  );
+};
+
+export default MovieList;
